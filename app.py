5cb13987e6a9852974f718dddb807852a7574df2
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-14 16:39:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-# Importing library
 import qrcode
 
-# Data to be encoded
-data = 'QR Code using make() function'
+qr = qrcode.QRCode(
+  version=1,
+  error_correction=qrcode.constants.ERROR_CORRECT_L,
+  box_size=10,
+  border=4,
+)
+qr.add_data("https://www.youtube.com/channel/UCDiSrTPJ4xaw8BP3nQj7c4Q")
 
-# Encoding data using make() function
-img = qrcode.make(data)
-
-# Saving as an image file
-img.save('MyQRCode1.png')
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qrcode.jpg")
