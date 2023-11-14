355b693697ffceb2468a2c45e7ed96e83e18279a
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-14 16:56:01 +0100

    s

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-qrcode.make()
+from qrcode import QRCode
+
+# Create a QR code object
+qr_code = QRCode()
+
+# Add data to the QR code
+qr_code.add_data("Hello, world!")
+
+# Generate the QR code
+qr_code.make()
