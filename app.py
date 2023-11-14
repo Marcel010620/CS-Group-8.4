e26fdaef6beef513b9509038937f01bf8f3e9e27
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-14 16:47:41 +0100

    s

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-qr = qrcode.QRCode(
-  version=1,
-  error_correction=qrcode.constants.ERROR_CORRECT_L,
-  box_size=10,
-  border=4,
-)
-qr.add_data("https://www.youtube.com/channel/UCDiSrTPJ4xaw8BP3nQj7c4Q")
-
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("qrcode.jpg")
+qrcode.make()
