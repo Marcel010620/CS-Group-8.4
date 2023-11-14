ccf067b72c43bbe0bace8cd1f77c2ad49af70e93
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-14 16:57:25 +0100

    s

--- QR Code File.py
+++ QR Code File.py
@@ -9,3 +9,16 @@ class QRCode:
         # Placeholder function for generating QR code
         # In a real-world application, you would call a library function here
         pass
+
+    
+    
+from qrcode import QRCode
+
+# Create a QR code object
+qr_code = QRCode()
+
+# Add data to the QR code
+qr_code.add_data("Hello, world!")
+
+# Generate the QR code
+qr_code.make()
