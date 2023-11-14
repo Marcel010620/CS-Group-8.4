0f6d2db1b082bcd1c8d6a63a56d8ebeeecb46de1
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-14 16:10:24 +0100

    QR Code

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import qrcode
 from PIL import Image
 
-class QRCodeGenerator:
-    def __init__(self, data):
-        self.data = data
+data = "https://www.google.com"
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
 
-    def generate_qr_code(self, filename, version=None, box_size=10, border=4):
-        qr = qrcode.QRCode(version, box_size=box_size, border=border)
-        qr.add_data(self.data)
-        qr.make(fit=True)
-
-        img = qr.make_image(fill_color="black", back_color="white")
-        img.save(filename)
-
-if __name__ == "__main__":
-    qr_code_generator = QRCodeGenerator("https://www.google.com")
-    qr_code_generator.generate_qr_code("google_qr_code.png")
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("google_qr_code.png")
