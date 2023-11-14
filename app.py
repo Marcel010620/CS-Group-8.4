25687c8c7442c314d088f990fae791361c3409e8
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-14 15:45:23 +0100

    QR Code Test

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 import qrcode
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data('Some data')
-qr.make(fit=True)
-
-img = qr.make_image(fill_color="black", back_color="white")
+img = qrcode.make('Some data here')
+type(img)  # qrcode.image.pil.PilImage
+img.save("some_file.png")
