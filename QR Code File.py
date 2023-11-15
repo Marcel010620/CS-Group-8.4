76b72e72969be20918f2cea801bdf1034029f474
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-15 14:02:41 +0100

    s

--- QR Code File.py
+++ QR Code File.py
@@ -5,33 +5,61 @@ import base64
 import io
 import streamlit as st
 
-def dict_to_qr_code(dict_data):
-    # Convert dictionary to a string
-    data_str = json.dumps(dict_data)
+import streamlit as st
+from datetime import datetime, timedelta
 
-    # Create QR Code instance
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
+options_Article = ["Pepper", "Milk"]
+selected_option_Article = st.selectbox("Choose your Article", options_Article)
+st.write('You selected:', selected_option_Article)
 
-    # Add data to QR Code
-    qr.add_data(data_str)
-    qr.make(fit=True)
+options_Owner = ["A", "B", "C"]
+selected_option_Owner = st.selectbox("Chosse the Owner", options_Owner)
+st.write("You selected", selected_option_Owner)
 
-    # Create QR Code image
-    img = qr.make_image(fill_color="black", back_color="white")
+today = datetime.now().date()
 
-    return img
+if selected_option_Article == "Pepper": 
+    Product_Code = "01"
+    calories = "0037"
+    Expiring_Date = (today + timedelta(days=10)).strftime("%d%m%Y")
+    if selected_option_Owner == "A": 
+        Owner_Nr = "01"
+    elif selected_option_Owner == "B": 
+        Owner_Nr = "02"
+    elif selected_option_Owner == "C":
+        Owner_Nr = "03"
+    Article_Code = str(Product_Code+Expiring_Date+calories+Owner_Nr)
 
-def decode_product_code(Article_Code):
-    # This function is a placeholder and should be replaced with your actual decoding function
-    product_number = "12345"
-    expiration_date = "2023-01-01"
-    calories = 100
-    product_owner = "John Doe"
+if selected_option_Article == "Milk": 
+    Product_Code = "02"
+    calories = "0400"
+    Expiring_Date = (today + timedelta(days=7)).strftime("%d%m%Y")
+    if selected_option_Owner == "A": 
+        Owner_Nr = "01"
+    elif selected_option_Owner == "B": 
+        Owner_Nr = "02"
+    elif selected_option_Owner == "C":
+        Owner_Nr = "03"
+    Article_Code = str(Product_Code+Expiring_Date+calories+Owner_Nr)
+
+if st.button('Reload your Article Code'):
+    # This will force the Streamlit app to rerun
+    st.experimental_rerun()
+
+st.write(Article_Code)
+
+from datetime import datetime, timedelta
+def decode_product_code(article_Code):
+    product_number = article_Code[:2]
+    expiration_date = article_Code[2:10]
+    calories = article_Code[10:14]
+    product_owner = article_Code[14:]
+
+    product_number = int(product_number)
+    
+    expiration_date = (datetime.strptime(expiration_date,"%d%m%Y")).strftime("%d.%m.%Y")
+    calories = calories.lstrip("0")
+    product_owner = "A" if product_owner == "01" else "B" if product_owner == "02" else "C" if product_owner == "03" else "No Owner"
 
     return {
         "Product Number": product_number,
@@ -40,6 +68,8 @@ def decode_product_code(Article_Code):
         "Product Owner": product_owner}
 
 st.write(decode_product_code(Article_Code))
+
+
 
 
 
