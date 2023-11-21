c3eb47ddf29289e645a49e66837cfb7c74b62602
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-21 14:02:38 +0100

    f

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from datetime import datetime, timedelta
 
 add_owner_button = st.button ("Add Owner")
 if add_owner_button: 
-    Owner = st.text_input("Enter Owner Nr1.", "")
+    Owner_name = st.text_input("Enter Owner Nr1.")
+
+    if Owner_name: 
+        owners_list = []
+        owners_list.append(Owner_name)
 
 
-
-
-st.title(f"This is the smart refrigerator of:blue{Owner}")
+st.title(f"This is the smart refrigerator of:blue{owners_list}")
