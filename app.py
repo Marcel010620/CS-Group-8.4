b788d37eecef39d223f5f9eeff2a722c95704a73
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-21 14:36:12 +0100

    fs

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import streamlit as st
 from datetime import datetime, timedelta
+st.title(f"his is the smart refrigerator of: {st.text_input("Your WG Name")}")
+
+add_owner_button = st.button("Add Owner")
+
+if add_owner_button: 
+    owners_list = []
+    owners_list.append(st.text_input("Owner Nr.1"))
+    add_another_owner_button = st.button ("Add another Owner")
+    if add_another_owner_button: 
+        owners_list.append(st.text_input("Owner Nr.2"))
 
 reset_owner_button = st.button("Reset Owners")
 
 if reset_owner_button: 
     owners_list = []
-
-add_owner_button = st.button("Add Owner")
-
-if add_owner_button:
-    owners_list = []
-    owners_list.append(st.text_input("Enter Owner Nr1."))
-
-# Display the list of owners outside the if block to avoid resetting the list on each iteration
-st.title(f"This is the smart refrigerator of: {' '.join(map(str, owners_list))}")
