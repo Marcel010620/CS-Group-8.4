832e83c08c5c48d66612f5718f655e2bc730804c
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-21 14:50:08 +0100

    df

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 from datetime import datetime, timedelta
+
+add_owner_button = st.button("Add owners here")
+remove_owner_button = st.button("Which Owner would you like to remove")
+add_item_button = st.button("Add an item to re fridge")
+remove_item_button = st.button("remove an item from the fridge")
+
+st.write(add_item_button,remove_item_button,add_owner_button,remove_owner_button)
+
+
 wg_name = st.text_input("Your WG name")
 
 st.title(f"his is the smart refrigerator of: {wg_name}")
