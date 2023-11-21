782dbc4b705bae7cc568c9e528460dc0d24f6294
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-21 13:14:22 +0100

    Af

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from datetime import datetime, timedelta
 
-Owner=["Eric", "Oli"]
+st.button ("Reset Owner")
+if st.button("Reset Owner"):
+    Owner = []
+    Owner.append(str(st.text_input("Owner Nr.1")))
+else: 
+    continue
+
 
 st.title(f"This is the smart refrigerator of:blue{Owner}")
