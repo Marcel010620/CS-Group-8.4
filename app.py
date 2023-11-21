e0172e0a933a4d527ff1f2e85a60326bf17871f5
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-21 15:00:21 +0100

    sgd

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
+#Import relevant libraries
 import streamlit as st
 from datetime import datetime, timedelta
-col1, col2, col3, col4 = st.columns(4)
-add_owner_button = col1.button("Add owners here")
-remove_owner_button = col2.button("Which Owner would you like to remove")
-add_item_button = col3.button("Add an item to re fridge")
-remove_item_button = col4.button("remove an item from the fridge")
 
-st.write(add_item_button,remove_item_button,add_owner_button,remove_owner_button)
-
-
+#insert the name of the fridge (for example your WG Name)
 wg_name = st.text_input("Your WG name")
 
-st.title(f"his is the smart refrigerator of: {wg_name}")
+#Display the title with the correct name given by a user input 
+st.title(f"this is the smart refrigerator of: {wg_name}")
 
-add_owner_button = st.button("Add Owner")
+#Initialize 4 columns to order 4 buttons in a row
+col1, col2, col3, col4 = st.columns(4)
 
-if add_owner_button: 
-    owners_list = []
-    owners_list.append(st.text_input("Owner Nr.1"))
-    add_another_owner_button = st.button ("Add another Owner")
-    if add_another_owner_button: 
-        owners_list.append(st.text_input("Owner Nr.2"))
+#Initialize buttons 
+add_item_button = col1.button("Add product")
+remove_item_button = col2.button("Remove product")
+add_owner_button = col3.button("Add owners")
+remove_owner_button = col4.button("remove owner")
 
-reset_owner_button = st.button("Reset Owners")
+#What happens if you press the add_item_button
+if add_item_button:
+    options_Article = ["Pepper", "Milk"]
+    selected_option_Article = st.selectbox("Choose your Article", options_Article)
+    st.write('You selected:', selected_option_Article)
 
-if reset_owner_button: 
-    owners_list = []
+    options_Owner = ["A", "B", "C"]
+    selected_option_Owner = st.selectbox("Chosse the Owner", options_Owner)
+    st.write("You selected", selected_option_Owner)
