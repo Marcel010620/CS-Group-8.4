f41aaa397d6901c147c2552080c45a4e679172d4
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-21 13:53:51 +0100

    fsf

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ st.button ("Reset Owner")
 if st.button("Reset Owner"):
     Owner = []
     Owner.append(str(st.text_input("Owner Nr.1")))
-else: 
-    continue
+
 
 
 st.title(f"This is the smart refrigerator of:blue{Owner}")
