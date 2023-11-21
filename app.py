24f4da6e01b3a3d9dbc70d14262b54e15da7c8fd
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-21 15:22:02 +0100

    sdf

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if "state" not in st.session_state:
 wg_name = st.text_input("Your WG name")
 
 #Display the title with the correct name given by a user input 
-st.title(f"this is the smart refrigerator of: {wg_name}")
+st.title(f"This is the smart refrigerator of: {wg_name}")
 
 #Initialize 4 columns to order 4 buttons in a row
 col1, col2, col3, col4 = st.columns(4)
@@ -27,14 +27,18 @@ remove_owner_button = col4.button("remove owner")
 
 #What happens if you press the add_item_button
 if add_item_button:
+    st.session_state.state.add_item_button_pressed = True
+
+# Display selectboxes only if add_item_button is pressed
+if st.session_state.state.add_item_button_pressed:
     options_Article = ["Pepper", "Milk"]
-    selected_option_Article = st.selectbox("Choose your Article", options_Article)
-    st.write('You selected:', selected_option_Article)
+    st.session_state.state.selected_option_Article = st.selectbox("Choose your Article", options_Article,
+                                                                  key="selected_option_Article")
+    st.write('You selected:', st.session_state.state.selected_option_Article)
 
     options_Owner = ["A", "B", "C"]
-    selected_option_Owner = st.selectbox("Chosse the Owner", options_Owner)
-    st.write("You selected", selected_option_Owner)
-
+    st.session_state.state.selected_option_Owner = st.selectbox("Choose the Owner", options_Owner,
+                                                                key="selected_option_Owner")
 #What happens if you press the remove_item_buttonv
 if remove_item_button:
     remove_options_article = ["Pepper", "Milk"] #this needs to be a list with all Procuts inside the fridge
