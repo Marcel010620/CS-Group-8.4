cb3ba18473ca45fef2b3fb3053d7f509bc4ff9fa
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-21 14:27:21 +0100

    fs

--- app.py
+++ app.py
@@ -10,10 +10,7 @@ add_owner_button = st.button("Add Owner")
 
 if add_owner_button:
     owners_list = []
-    owner_name = st.text_input("Enter Owner Nr1.")
-    owners_list.append(owner_name)
-
-
+    owners_list.append(st.text_input("Enter Owner Nr1."))
 
 # Display the list of owners outside the if block to avoid resetting the list on each iteration
 st.title(f"This is the smart refrigerator of: {' '.join(map(str, owners_list))}")
