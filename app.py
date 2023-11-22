e81e9573b2912657f62e0811d5b2e296f74e3d96
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-22 18:52:06 +0100

    hgf

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 
 # Initialize session state
 if "selected_options" not in st.session_state:
-    st.session_state.selected_options = {"Article": None, "Owner": None, "show_select_boxes": False}
+    st.session_state.selected_options = {"Article": None, "Owner": None, "show_select_boxes": {"add_item_button": False, "remove_item_button": False, "add_owner_button": False, "remove_owner_button": False}}
 
 # Insert the name of the fridge (for example, your WG Name)
 wg_name = st.text_input("Your WG name")
@@ -18,11 +18,14 @@ col1, col2, col3, col4 = st.columns(4)
 # What happens if you press the add_item_button
 add_item_button = col1.button("Add product")
 if add_item_button:
-    options_Article = ["Pepper", "Milk"]
-    st.session_state.selected_options["show_select_boxes"] = True
+    st.session_state.selected_options["show_select_boxes"]["add_item_button"] = True
+    # Reset other buttons
+    st.session_state.selected_options["show_select_boxes"]["remove_item_button"] = False
+    st.session_state.selected_options["show_select_boxes"]["add_owner_button"] = False
+    st.session_state.selected_options["show_select_boxes"]["remove_owner_button"] = False
 
 # Show select boxes if the flag is True
-if st.session_state.selected_options["show_select_boxes"]:
+if st.session_state.selected_options["show_select_boxes"]["add_item_button"]:
     options_Article = ["Pepper", "Milk"]
     st.session_state.selected_options["Article"] = st.selectbox("Choose your Article", options_Article,
                                                                 key="article_selectbox")
@@ -36,13 +39,29 @@ if st.session_state.selected_options["show_select_boxes"]:
 # What happens if you press the remove_item_button
 remove_item_button = col2.button("Remove product")
 if remove_item_button:
+    st.session_state.selected_options["show_select_boxes"]["remove_item_button"] = True
+    # Reset other buttons
+    st.session_state.selected_options["show_select_boxes"]["add_item_button"] = False
+    st.session_state.selected_options["show_select_boxes"]["add_owner_button"] = False
+    st.session_state.selected_options["show_select_boxes"]["remove_owner_button"] = False
+
+# Show select boxes if the flag is True
+if st.session_state.selected_options["show_select_boxes"]["remove_item_button"]:
     remove_options_article = ["Pepper", "Milk"]  # This needs to be a list with all Products inside the fridge
     removed_options_article = st.selectbox("Choose the articles you want to remove", remove_options_article)
     st.write("You removed", removed_options_article)
-    
+
 # What happens if you press the add_owner_button
 add_owner_button = col3.button("Add owners")
 if add_owner_button:
+    st.session_state.selected_options["show_select_boxes"]["add_owner_button"] = True
+    # Reset other buttons
+    st.session_state.selected_options["show_select_boxes"]["add_item_button"] = False
+    st.session_state.selected_options["show_select_boxes"]["remove_item_button"] = False
+    st.session_state.selected_options["show_select_boxes"]["remove_owner_button"] = False
+
+# Show select boxes if the flag is True
+if st.session_state.selected_options["show_select_boxes"]["add_owner_button"]:
     new_owner = st.text_input("Enter new owner")
     if new_owner:
         st.session_state.selected_options["Owner"] = new_owner
@@ -51,6 +70,14 @@ if add_owner_button:
 # What happens if you press the remove_owner_button
 remove_owner_button = col4.button("Remove owner")
 if remove_owner_button:
+    st.session_state.selected_options["show_select_boxes"]["remove_owner_button"] = True
+    # Reset other buttons
+    st.session_state.selected_options["show_select_boxes"]["add_item_button"] = False
+    st.session_state.selected_options["show_select_boxes"]["remove_item_button"] = False
+    st.session_state.selected_options["show_select_boxes"]["add_owner_button"] = False
+
+# Show select boxes if the flag is True
+if st.session_state.selected_options["show_select_boxes"]["remove_owner_button"]:
     owners_list = ["A", "B", "C"]  # Replace with your list of owners
     removed_owner = st.selectbox("Choose the owner to remove", owners_list)
     if removed_owner:
