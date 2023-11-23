86d8e49edfd830122fccae7527f30a2c9042b508
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-23 10:20:30 +0100

    s

--- QR Code File.py
+++ QR Code File.py
@@ -1,6 +1,6 @@
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
 
 # Define your data
 data = {'Article': ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'],
@@ -11,9 +11,10 @@ df = pd.DataFrame(data)
 
 # Sort data by quantity in descending order
 df = df.sort_values(by='Quantity', ascending=False)
+df = df.reset_index(drop=True)  # Reset the index for correct bar positioning
 
 # Set figure size
-plt.figure(figsize=(10,5))
+fig, ax = plt.subplots(figsize=(10, 5))
 
 # Set the color of the bar for each category
 color_dict = {'Fruit': 'c'}
@@ -21,29 +22,21 @@ color_dict = {'Fruit': 'c'}
 # Define bar positions on x axis
 bar_positions = range(len(df['Article']))
 
-# Create bars
+# Create bars with small spaces
+bar_width = 0.8  # Adjust the width as needed
 for i, row in df.iterrows():
-    plt.bar(bar_positions[i], row['Quantity'], color=color_dict[row['Category']], alpha=0.6)
-
-# Create bar labels
-for i, row in df.iterrows():
-    plt.text(bar_positions[i], row['Quantity'] + 0.2, row['Article'], fontsize=12)
-
+    ax.bar(bar_positions[i], row['Quantity'], color=color_dict[row['Category']], alpha=0.6, width=bar_width)
 
 # Set axes limits
-plt.ylim(0, df['Quantity'].max() + 2)
+ax.set_ylim(0, df['Quantity'].max() + 2)
 
-# Create custom x-axis ticks and labels for the articles
-article_ticks = []
-article_labels = []
-
+# Add labels above the bars
 for i, row in df.iterrows():
-    article_ticks.append(i + 0.5)
-    article_labels.append(row['Article'])
+    ax.text(bar_positions[i], row['Quantity'] + 0.2, row['Article'], fontsize=12, ha='center')
 
-plt.xticks(article_ticks, article_labels, rotation=90)
+# Remove x-axis labels below the axis
+ax.set_xticks([])
+ax.set_xticklabels([])
 
-# Show the plot
-plt.show()
-
-
+# Display the plot using Streamlit
+st.pyplot(fig)
