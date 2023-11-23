1b92335ccc96d4d03817847de258e417425aa446
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-23 16:20:01 +0100

    s

--- Visualization.py
+++ Visualization.py
@@ -42,48 +42,36 @@ st.altair_chart(chart, use_container_width=True)
 
 import streamlit as st
 import pandas as pd
-import altair as alt
-import numpy as np
+from datetime import datetime, timedelta
 
-# Sample data for different selections
-data_dict = {
-    'Option 1': [10, 15, 20, 25, 30],
-    'Option 2': [5, 10, 15, 20, 25],
-    'Option 3': [8, 12, 16, 20, 24],
+# Sample data
+data = {
+    'Article': ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'],
+    'Quantity': [10, 5, 7, 3, 2],
+    'Category': ['Fruit', 'Fruit', 'Fruit', 'Fruit', 'Fruit'],
+    'Owner': ['A', 'B', 'C', 'A', 'B'],
+    'Expiration Date': [datetime(2023, 12, 1), datetime(2023, 12, 3), datetime(2023, 12, 5), datetime(2023, 11, 25), datetime(2023, 12, 7)],
 }
 
+df = pd.DataFrame(data)
+
 # Create a Streamlit app
-st.title('Dropdown Selection and Bar Chart')
+st.title('List Based on Dropdown Selection')
 
-# Create a dropdown to select an option
-selected_option = st.selectbox('Select an option:', list(data_dict.keys()))
+# Create a dropdown to select either "Owner" or "Expires soon"
+selection_option = st.selectbox('Select an option:', ['Owner', 'Expires soon'])
 
-# Get data for the selected option
-selected_data = data_dict[selected_option]
+# Display the selected option
+st.write(f'Selected option: {selection_option}')
 
-# Create a DataFrame for Altair
-df = pd.DataFrame({'X': range(1, len(selected_data)+1), 'Y': selected_data})
+# Based on the selected option, create and display the corresponding list
+if selection_option == 'Owner':
+    owners_list = df['Owner'].unique()
+    selected_list = st.multiselect('Select owners:', owners_list)
+    st.write(f'Selected owners: {selected_list}')
 
-# Define the step size on the x-axis
-step_size = 1  # Change this value according to your preference
-
-# Create a bar chart with Altair and set the step size on the x-axis
-chart = alt.Chart(df).mark_bar().encode(
-    x=alt.X('X:O', axis=alt.Axis(step=step_size)),  # 'O' indicates ordinal scale
-    y='Y',
-    color=alt.value('blue')
-).properties(
-    width=400,
-    height=300,
-    title=f'Bar Chart - {selected_option}'
-)
-
-# Display the bar chart using Streamlit
-st.altair_chart(chart, use_container_width=True)
-
-# Create a list based on the selected option
-selected_list = [f'{selected_option} - Item {i+1}' for i in range(len(selected_data))]
-
-# Display the created list
-st.write("Created list based on the selected option:")
-st.write(selected_list)
+elif selection_option == 'Expires soon':
+    expiration_threshold = datetime.now() + timedelta(days=2)
+    expiring_articles = df[df['Expiration Date'] <= expiration_threshold]['Article'].tolist()
+    st.write("Articles that expire soon:")
+    st.write(expiring_articles)
