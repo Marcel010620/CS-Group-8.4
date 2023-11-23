37d1ddc5a94cedb78a5da7a27f8cb1600b3f8cfa
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-11-23 15:49:19 +0100

    s

--- Visualization.py
+++ Visualization.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import altair as alt
 import numpy as np
 
 # Sample data for different selections
 data_dict = {
-    'Option 1': [10, 15, 20, 25, 30, 35],
-    'Option 2': [5, 10, 15, 20, 25, 30],
-    'Option 3': [8, 12, 16, 20, 24, 28],
+    'Option 1': [10, 15, 20, 25, 30],
+    'Option 2': [5, 10, 15, 20, 25],
+    'Option 3': [8, 12, 16, 20, 24],
 }
 
 # Create a Streamlit app
@@ -19,12 +19,19 @@ selected_option = st.selectbox('Select an option:', list(data_dict.keys()))
 # Get data for the selected option
 selected_data = data_dict[selected_option]
 
-# Create a bar chart based on the selected data
-fig, ax = plt.subplots()
-ax.bar(range(1, len(selected_data)+1), selected_data, color='blue')
-ax.set_xlabel('X-Axis Label')
-ax.set_ylabel('Y-Axis Label')
-ax.set_title(f'Bar Chart - {selected_option}')
+# Create a DataFrame for Altair
+df = pd.DataFrame({'X': range(1, len(selected_data)+1), 'Y': selected_data})
+
+# Create a bar chart with Altair
+chart = alt.Chart(df).mark_bar().encode(
+    x='X',
+    y='Y',
+    color=alt.value('blue')
+).properties(
+    width=400,
+    height=300,
+    title=f'Bar Chart - {selected_option}'
+)
 
 # Display the bar chart using Streamlit
-st.pyplot(fig)
+st.altair_chart(chart, use_container_width=True)
