9483b18818df1e456fdf48fc354a83854e76af57
Ericbangerter <147808442+Ericbangerter@users.noreply.github.com>
2023-11-28 08:37:51 +0100

    sdfs

---
+++ Zusammentrag.py
@@ -0,0 +1,18 @@
+#Import relevant libraries
+import streamlit as st
+from datetime import datetime, timedelta
+
+#initialize classes & sublcasses 
+
+    #Initilize Functions
+
+#Visualization 
+
+#User input 
+    #Buttons
+    #Selectboxes 
+    #Encode
+        #--> Lisst append
+    #Decode
+        #--> Show item
+    #Visulization of product/items
