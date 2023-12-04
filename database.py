54d261fcf187a39363f50a41ef2b42bcff07631d
Marcel010620 <147808596+Marcel010620@users.noreply.github.com>
2023-12-04 19:49:31 +0100

    s

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
-import streamlit as st
-import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
+import streamlit as st
+import pandas as pd
+
 
 # Google Sheets Authentifizierungs-Scope
 SCOPES = [
